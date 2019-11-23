db7c9c9f8e01780071dd025be3670187cf86f5b7
Kongfi <andreas@dennis-t.dk>
2019-11-23 22:01:41 +0100

    InitialCommit

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            // Send e-mail
+            MailMessage message = new MailMessage();
+            message.To.Add("eaaawkn@students.eaaa.dk");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            GuidUdi currentPageUdi = new GuidUdi(CurrentPage.ContentType.ItemType.ToString(), CurrentPage.Key);
+
+            IContent msg = Services.ContentService.CreateContent(model.Subject, currentPageUdi, "message");
+            msg.SetValue("messageName", model.Name);
+            msg.SetValue("email", model.Email);
+            msg.SetValue("subject", model.Subject);
+            msg.SetValue("messageContent", model.Message);
+            msg.SetValue("umbracoNaviHide", true);
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("kongfitest@gmail.com", "B3rzV2dPnywp"); 
+
+                smtp.Send(message);
+            }
+
+            TempData["success"] = true;
+
+            Services.ContentService.Save(msg);
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
